758b6294de73b935feb58a5e8a10f36f27b0b45e
JacobF94 <jacob.frensley@gmail.com>
2021-12-02 15:23:41 -0600

    completed challenge

--- shopping.js
+++ shopping.js
@@ -26,4 +26,52 @@ const shoppingList = [
     }
 ]
 
-console.log(shoppingList)
+const addToShoppingList = (newItem) => {
+    const newId = shoppingList.length + 1
+    newItem.id = newId
+    newItem.dateCreated = Date()
+    shoppingList.push(newItem)
+}
+
+const greekYogurt = {
+    name: "greek yogurt",
+    price: 7.50
+}
+
+const paperTowels = {
+    name: "paper towels",
+    price: 12
+}
+
+const scentedCandle = {
+    name: "candle",
+    price: 10
+}
+
+const freshSalmon = {
+    name: "salmon",
+    price: 16
+}
+
+const bagOfChips = {
+    name: "pringles",
+    price: 5
+}
+addToShoppingList(greekYogurt)
+addToShoppingList(paperTowels)
+addToShoppingList(scentedCandle)
+addToShoppingList(freshSalmon)
+addToShoppingList(bagOfChips)
+
+const expensiveItems = () => {
+    let expensiveList = []
+    for (const item of shoppingList) {
+
+        if (item.price > 8) {
+            expensiveList.push(item)
+        }
+    }
+    return expensiveList
+}
+
+console.log(expensiveItems())
