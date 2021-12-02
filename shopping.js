1be889f6d7e1b79f152130b1d879b832263ad458
JacobF94 <jacob.frensley@gmail.com>
2021-12-02 11:50:42 -0600

    first commit

---
+++ shopping.js
@@ -0,0 +1,29 @@
+const shoppingList = [
+    {
+        id: 1,
+        name: "lentils",
+        price: 3
+    },
+    {
+        id: 2,
+        name: "granola bars",
+        price: 3.5
+    },
+    {
+        id: 3,
+        name: "oat milk",
+        price: 4
+    },
+    {
+        id: 4,
+        name: "cheerios",
+        price: 4.5
+    },
+    {
+        id: 5,
+        name: "oranges",
+        price: 5
+    }
+]
+
+console.log(shoppingList)
